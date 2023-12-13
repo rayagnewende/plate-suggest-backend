d160361a05750dc9e1c6cb544112b9780d8ad739
kabore r evariste <kaboreva@gmail.com>
2023-12-13 15:53:07 +0100

    ajout d'une expression regulière pour la validation de lemail

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,8 @@ router.post('/signup', (req, res) => {
   const { username, email, password} = req.body ; 
   const hash = bcrypt.hashSync(password, 10);
   const token = uid2(32); 
-  const EMAIL_REGEX = /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+  const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; 
+
 
   User.find()
       .then( users => {
